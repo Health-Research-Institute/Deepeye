e0e95b0e7fea213fa8ac43c891204042ae6abe9a
Leonid Naimark <leonidnaimark@gmail.com>
2023-03-17 13:30:04 -0400

    Images are loaded in a loop and full segmentation can be looked through to think about patterns

--- OCT-Retinal-Layer-Segmenter/player.py
+++ OCT-Retinal-Layer-Segmenter/player.py
@@ -12,7 +12,8 @@ np.set_printoptions(threshold=sys.maxsize)
 
 # PARAMETERS 
 # Image directory
-imagesPath = 'OCT-Retinal-Layer-Segmenter/trainingSet/origImages/'
+# imagesPath = 'OCT-Retinal-Layer-Segmenter/trainingSet/origImages/'
+imagesPath = '../Images/CT_RETINA/DIABETR_107/'
 imageIds = next(os.walk(imagesPath))[2]
 #Parameters to use with player
 sizeX = 640
@@ -43,40 +44,38 @@ model.load_weights('OCT-Retinal-Layer-Segmenter/retina_segmentation_8_layer.hdf5
 # End LOAD MODEL
 
 # LOOP VIA IMAGES 
-testNumber = 6
-valImage=thirdImages[testNumber,:,:,:] #size: [sizeX, sizeY, 1] 
-testInput=np.expand_dims(valImage, 0) #size: [1, sizeX, sizeY, 1] 
+#for i in range(nImages):
+for i in range(0,8):
+    valImage=thirdImages[i,:,:,:] #size: [sizeX, sizeY, 1] 
+    testInput=np.expand_dims(valImage, 0) #size: [1, sizeX, sizeY, 1] 
+    prediction = (model.predict(testInput))  #size [1, sizeX, sizeY, 9] 
+    predicted_img = np.argmax(prediction, axis=3)[0,:,:] #size: [sizeX, sizeY] 
 
-prediction = (model.predict(testInput))  #size [1, sizeX, sizeY, 9] 
-predicted_img = np.argmax(prediction, axis=3)[0,:,:] #size: [sizeX, sizeY] 
-
-
-
-# PLOT INFORMATION
-plt.figure(figsize=(20, 10))
-plt.subplot(261)
-plt.title('Testing Image')
-plt.imshow(valImage[:,:,0])
-plt.subplot(262)
-plt.imshow(prediction[0,:,:,0], cmap='jet')
-plt.title('Background Predicition ')
-plt.subplot(263)
-plt.imshow(prediction[0,:,:,5], cmap='jet')
-plt.subplot(264)
-plt.imshow(prediction[0,:,:,3], cmap='jet')
-plt.subplot(265)
-plt.imshow(prediction[0,:,:,6], cmap='jet')
-plt.subplot(266)
-plt.imshow(prediction[0,:,:,1], cmap='jet')
-plt.subplot(267)
-plt.imshow(prediction[0,:,:,7], cmap='jet')
-plt.subplot(268)
-plt.imshow(prediction[0,:,:,4], cmap='jet')
-plt.subplot(269)
-plt.imshow(prediction[0,:,:,2], cmap='jet')
-plt.subplot(2,6,10)
-plt.imshow(prediction[0,:,:,8], cmap='jet')
-plt.subplot(2,6,11)
-plt.title('Combines Prediction')
-plt.imshow(predicted_img, cmap='jet')
-plt.show()
+    # PLOT INFORMATION
+    plt.figure(figsize=(20, 10))
+    plt.subplot(261)
+    plt.title('Testing Image')
+    plt.imshow(valImage[:,:,0])
+    plt.subplot(262)
+    plt.imshow(prediction[0,:,:,0], cmap='jet')
+    plt.title('Background Predicition ')
+    plt.subplot(263)
+    plt.imshow(prediction[0,:,:,5], cmap='jet')
+    plt.subplot(264)
+    plt.imshow(prediction[0,:,:,3], cmap='jet')
+    plt.subplot(265)
+    plt.imshow(prediction[0,:,:,6], cmap='jet')
+    plt.subplot(266)
+    plt.imshow(prediction[0,:,:,1], cmap='jet')
+    plt.subplot(267)
+    plt.imshow(prediction[0,:,:,7], cmap='jet')
+    plt.subplot(268)
+    plt.imshow(prediction[0,:,:,4], cmap='jet')
+    plt.subplot(269)
+    plt.imshow(prediction[0,:,:,2], cmap='jet')
+    plt.subplot(2,6,10)
+    plt.imshow(prediction[0,:,:,8], cmap='jet')
+    plt.subplot(2,6,11)
+    plt.title('Combines Prediction')
+    plt.imshow(predicted_img, cmap='jet')
+    plt.show()
