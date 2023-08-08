9e00bdfcd7d60655bbe2c3a7d7ab0103437c4fc9
Leonid Naimark <leonidnaimark@gmail.com>
2023-08-08 15:50:00 -0400

    analysis Function addition

--- densenetClassifier/analysisFunctions.py
+++ densenetClassifier/analysisFunctions.py
@@ -1,5 +1,6 @@
 # Import libraries
 import torch
+import numpy as np
 
 #Create Image Stack
 def compute_score_with_logits(logits, labels, device):
@@ -15,3 +16,19 @@ def compute_score_with_logits(logits, labels, device):
     scores = (one_hots * labels)
 
     return scores
+
+
+def analyseTest(tensorIn):
+    prob = tensorIn.cpu().numpy()
+    prob = np.round(100*prob)/100
+      
+    nCl = len(prob[0])
+    #initialize probability scorer
+    fitN = np.zeros(nCl)
+    #find maximum probability index 
+    mpi =np.argmax(prob, axis=-1) #8-element vector w highest prob
+
+    for j in range(nCl):
+        fitN[j] = sum(np.equal(mpi, j))
+
+    return fitN
