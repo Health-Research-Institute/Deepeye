631125d9d631d4b0438bce96c0ce1f9effa63895
Leonid Naimark <leonidnaimark@gmail.com>
2023-08-08 11:25:11 -0400

    New Segment Player, updated dir struct

---
+++ OCT-Retinal-Layer-Segmenter/player_Stage1.py
@@ -0,0 +1,59 @@
+from simple_unet import multi_unet_model
+from keras.utils import normalize
+import os
+import glob
+import cv2
+import numpy as np
+import sys
+
+testClass = 1
+
+classNames = ['AGE_RMD','CSR','DIABETR','MACHOLE','NORMAL']
+classDirs = ['AGE_RMD_55','CSR_102','DIABETR_107','MACHOLE_102','NORMAL_206']
+cAbbr = ['AR', 'CR', 'DR', 'MH', 'NR']
+
+imagesPathRead = '../Images/CT_RETINA/' + classDirs[testClass] + '/' + cAbbr[testClass] + 'Test'
+imagesPathWrite ='../Images/CT_RETINA/' + classDirs[testClass] + '/' + cAbbr[testClass] + 'TempRes/'
+imageIds = next(os.walk(imagesPathRead))[2]
+#Parameters to use with player
+sizeX = 640
+sizeY = 640
+nLayers= 9 # Number of classes for segmentation
+layersNames = ['BCG', 'NFL', 'GCL', 'INL', 'OPL' , 'ONL', 'ELZ', 'RPE', 'CHO'] #layers names 
+nOrder =  [0, 5, 3, 6, 1, 7, 4, 2, 8] #Sequence of layers per model:
+
+# PREPARING IMAGES FOR PLAYER
+initImages = []
+for directoryPath in glob.glob(imagesPathRead):
+    for imgPath in glob.glob(os.path.join(directoryPath, "*.jpeg")):
+        img = cv2.imread(imgPath, 0)       
+        img = cv2.resize(img, (sizeY, sizeX))
+        initImages.append(img)  
+     
+initImages = np.array(initImages) #size is [nImages, sizeX, sizeY]  
+nImages, w, h = initImages.shape
+#print( 'number of images', nImages)
+secImages = np.expand_dims(initImages, axis=3) #size: [nImages, sizeX, sizeY, 1]  
+thirdImages = normalize(secImages, axis=1) #size: [nImages, sizeX, sizeY, 1] 
+# End PREPARING 
+
+# LOAD MODEL
+model = multi_unet_model(n_classes=nLayers, IMG_HEIGHT=sizeY, IMG_WIDTH=sizeX, IMG_CHANNELS=1)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.load_weights('OCT-Retinal-Layer-Segmenter/retina_segmentation_8_layer.hdf5')
+# End LOAD MODEL
+
+# LOOP VIA IMAGES 
+for i in range(0,10):
+#for i in range(0,len(imageIds)):
+    valImage=thirdImages[i,:,:,:] #size: [sizeX, sizeY, 1] 
+    testInput=np.expand_dims(valImage, 0) #size: [1, sizeX, sizeY, 1] 
+    prediction = (model.predict(testInput))  #size [1, sizeX, sizeY, 9] 
+    predicted_img = np.argmax(prediction, axis=3)[0,:,:] #size: [sizeX, sizeY] 
+
+    #loop through nine layers 
+    for j in range(0,9):        
+        layer01 = prediction[0,:,:,nOrder[j]] *255
+        layerTest = layer01.round() 
+        cv2.imwrite(imagesPathWrite + classNames[testClass] + str (i+1) + '_' + str(j) + '_' + layersNames[j] + '.jpg', layerTest)
+ 
