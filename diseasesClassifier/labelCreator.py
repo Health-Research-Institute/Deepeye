2f97a300c89e9bdc13e899e380f8179b27ab3911
Vladimir Kluzner <vkluzner@jhu.edu>
2023-09-15 18:36:53 -0700

    Fixes following the firs CI run

--- diseasesClassifier/labelCreator.py
+++ diseasesClassifier/labelCreator.py
@@ -1,12 +1,6 @@
 # test file
 import cv2
 
-import torch
-from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor
-
 #define data directory 
 dataDir =  "../Images/STARE/"
 
